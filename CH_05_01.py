cced85360f3fa71224424a934d24b66dea8832bc
Rony Sheer <ronysheer@gmail.com>
2020-12-13 13:08:50 +0200

    05_03

--- CH_05_01.py
+++ CH_05_01.py
@@ -1,24 +1,10 @@
-zero_to_nine = [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9]
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+zero_to_nine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print("=====iterating [0, ..., 9]=====")
 
 for num in zero_to_nine:
     print(num)
 
-print("=====using make_range=====")
-
-def make_range(num):
-    if num < 0:
-        raise Exception("number is too small.")
-    count = 0
-    while count < num:
-        yield count
-        count += 1
-
-
-for num in make_range(9):
-    print(num)
 
 print("=====using range=====")
 
@@ -32,18 +18,3 @@ AGES = [34, 25, 55, 42]
 
 for name, age in zip(NAMES, AGES):
     print(f"{name} is {age} years old.")
-
-print("=====iterating index, name with enumerate=====")
-
-for i, name in enumerate(NAMES):
-    print(i, name)
-
-print("=====reversed(zero_to_nine)=====")
-
-for num in reversed(zero_to_nine):
-    print(num)
-
-print("=====sorted(AGES)=====")
-
-for num in sorted(AGES):
-    print(num)
--- CH_05_02.py
+++ CH_05_02.py
@@ -1,8 +1,17 @@
-
-print("=====iterating zip(NAMES, AGES)=====")
-
 NAMES = ["Jake", "Jay", "Jim", "Jill"]
 AGES = [34, 25, 55, 42]
+
+print("=====reversed(AGES:[34, 25, 55, 42])=====")
+
+for num in reversed(AGES):
+    print(num)
+
+print("=====sorted(AGES)=====")
+
+for num in sorted(AGES):
+    print(num)
+
+print("=====iterating zip(NAMES, AGES)=====")
 
 for name, age in zip(NAMES, AGES):
     print(f"{name} is {age} years old.")
@@ -11,14 +20,3 @@ print("=====iterating index, name with enumerate=====")
 
 for i, name in enumerate(NAMES):
     print(i, name)
-
-print("=====reversed([0 ,1, 2, 3, 4])=====")
-
-for num in reversed([0 ,1, 2, 3, 4):
-    print(num)
-
-print("=====sorted(AGES)=====")
-
-for num in sorted(AGES): # returns list
-    print(num)
-
